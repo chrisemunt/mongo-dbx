534bf59966b920830a2fd6f2821983fd3faa0517
chrisemunt <cmunt@mgateway.com>
2019-08-29 16:58:30 +0100

    Add files via upload

--- src/mongo/bcon.c
+++ src/mongo/bcon.c
@@ -42,9 +42,11 @@ bcon_token_t bcon_token(char *s);
 bcon_token_t bcon_token(char *s) {
     if (s == 0) return Token_EOD;
     switch (s[0]) {
-    case ':': if (s[1] != '\0' && s[2] != '\0' && s[3] != '\0' && s[4] == '\0' &&
-                      s[3] == ':' && (s[1] == '_' || s[1] == 'P' || s[1] == 'R'))
-            return Token_Typespec; break;
+    case ':':
+      if (s[1] != '\0' && s[2] != '\0' && s[3] != '\0' && s[4] == '\0' &&s[3] == ':' && (s[1] == '_' || s[1] == 'P' || s[1] == 'R')) {
+            return Token_Typespec;
+      }
+      break;
     case '{': if (s[1] == '\0') return Token_OpenBrace; break;
     case '}': if (s[1] == '\0') return Token_CloseBrace; break;
     case '[': if (s[1] == '\0') return Token_OpenBracket; break;
